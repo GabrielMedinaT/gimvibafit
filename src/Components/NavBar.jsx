3f7c50ed3e5b6cfccb30800794f9a78e2514b0e4
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-03-27 18:33:55 +0000

    Adaptacion del navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,57 +4,71 @@ import "./navBar.css";
 
 export const navBar = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <Link to="/Home">
-            {" "}
-            <button data-text="Awesome" class="button">
-              Inicio
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/Actividades">
-            {" "}
-            <button data-text="Awesome" class="button">
-              <span class="hover-text" aria-hidden="true">
-                &nbsp;Actividades&nbsp;
-              </span>
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/Contacto">
-            {" "}
-            <button data-text="Awesome" class="button">
-              <span class="hover-text" aria-hidden="true">
-                &nbsp;Contacto&nbsp;
-              </span>
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/Acerca">
-            {" "}
-            <button data-text="Awesome" class="button">
-              <span class="hover-text" aria-hidden="true">
-                &nbsp;Acerca&nbsp;
-              </span>
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/Login">
-            {" "}
-            <button data-text="Awesome" class="button">
-              <span class="hover-text" aria-hidden="true">
-                &nbsp;Log In&nbsp;
-              </span>
-            </button>
-          </Link>
-        </li>
-      </ul>
+    <div className="navBar">
+      <div className="nav">
+        <h1>VibaFit</h1>
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-btn">
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link to="/Home">
+                {" "}
+                <button data-text="Awesome" class="button">
+                  Inicio
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Actividades">
+                {" "}
+                <button data-text="Awesome" class="button">
+                  <span class="hover-text" aria-hidden="true">
+                    Actividades
+                  </span>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Contacto">
+                {" "}
+                <button data-text="Awesome" class="button">
+                  <span class="hover-text" aria-hidden="true">
+                    Contacto
+                  </span>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Acerca">
+                {" "}
+                <button data-text="Awesome" class="button">
+                  <span class="hover-text" aria-hidden="true">
+                    Acerca
+                  </span>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Login">
+                {" "}
+                <button data-text="Awesome" class="button">
+                  <span class="hover-text" aria-hidden="true">
+                    Log In
+                  </span>
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
