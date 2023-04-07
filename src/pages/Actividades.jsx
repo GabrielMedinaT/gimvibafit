7296c3f2e4691bb570707473eabd6dde9819ec95
Daniel <danielojedamartel12@gmail.com>
2023-04-07 22:37:27 +0100

    Martes 2º hora hecho de actividades

--- src/pages/Actividades.jsx
+++ src/pages/Actividades.jsx
@@ -10,45 +10,80 @@ const Actividades = () => {
         {/* Nombrarmos a los div de los dias de la siguiente mandera dlunes, dmartes etc */}
         <h3>Lunes</h3>
       </div>
-      <div className="hl1" id="horasL">
-        <h3>Funcional</h3>
-        <h3>Core</h3>
-        <h3>Tabata</h3>
-        <h3>Cardio</h3>
+      <div id="hovHor">
+        <div>
+          <h3 className="horasMov">9:30</h3>
+        </div>
+        <div className="hl1" id="horasL">
+          <h3>Funcional</h3>
+          <h3>Core</h3>
+          <h3>Tabata</h3>
+          <h3>Cardio</h3>
+        </div>
       </div>
-      <div className="hl2" id="horasL">
-        <h3>Funcional</h3>
+      <div id="hovHor">
+        <div>
+          <h3 className="horasMov">10:30</h3>
+        </div>
+        <div className="hl2" id="horasL">
+          <h3>Funcional</h3>
+        </div>
       </div>
-      <div className="hl3" id="horasL">
-        <h3>Tabata</h3>
-        <h3>Strong</h3>
-        <h3>Funcional</h3>
-        <h3>Cross</h3>
+      <div className="hl3" id="hovHor">
+        <div>
+          <h3 className="horasMov">18:00</h3>
+        </div>
+        <div className="hl3" id="horasL">
+          <h3>Tabata</h3>
+          <h3>Strong</h3>
+          <h3>Funcional</h3>
+          <h3>Cross</h3>
+        </div>
       </div>
-      <div className="hl4" id="horasL">
-        <h3>Ritmos Mixtos</h3>
-        <h3>Funcional</h3>
-        <h3>Step/Latin Fit</h3>
-        <h3>Dance Fit</h3>
+      <div className="hl4" id="hovHor">
+        <div>
+          <h3 className="horasMov">19:00</h3>
+        </div>
+        <div className="hl4" id="horasL">
+          <h3>Ritmos Mixtos</h3>
+          <h3>Funcional</h3>
+          <h3>Step/Latin Fit</h3>
+          <h3>Dance Fit</h3>
+        </div>
       </div>
-      <div className="hl5" id="horasL">
-        <h3>Zumba</h3>
-        <h3>Core</h3>
-        <h3>Dance Fit</h3>
-        <h3>Latin Fit</h3>
+      <div className="hl5" id="hovHor">
+        <div>
+          <h3 className="horasMov">20:00</h3>
+        </div>
+        <div className="hl5" id="horasL">
+          <h3>Zumba</h3>
+          <h3>Core</h3>
+          <h3>Dance Fit</h3>
+          <h3>Latin Fit</h3>
+        </div>
       </div>
       {/* MARTES */}
       <div className="dmartes" id="dia">
         <h3>Martes</h3>
       </div>
-      <div className="hm1" id="horas">
-        <h3>Dorsal</h3>
-        <h3>Fitball</h3>
-        <h3>Fullbody</h3>
-        <h3>Body up</h3>
+      <div className="hm1">
+        <div className="horasMov">
+          <h3>9:30</h3>
+        </div>
+        <div className="hm1" id="horas">
+          <h3>Dorsal</h3>
+          <h3>Fitball</h3>
+          <h3>Fullbody</h3>
+          <h3>Body up</h3>
+        </div>
       </div>
-      <div className="hm2" id="horas">
-        <h3>GAP</h3>
+      <div className="hm2">
+        <div className="horasMov">
+          <h3>10:30</h3>
+        </div>
+        <div className="hm2" id="horas">
+          <h3>GAP</h3>
+        </div>
       </div>
       <div className="hm3" id="horas">
         <h3>Pilates</h3>
@@ -64,13 +99,17 @@ const Actividades = () => {
       <div className="dmiercoles" id="dia">
         <h3>Miercoles</h3>
       </div>
-
+      {/* <div> */}
+      {/* <div className="horasMov">
+          <h3>9:30</h3>
+        </div> */}
       <div className="hx1" id="horas">
         <h3>Cardio Box</h3>
         <h3>Trampolín</h3>
         <h3>Comba</h3>
         <h3>Metcom</h3>
       </div>
+      {/* </div> */}
       <div className="hx2" id="horas">
         <h3>METCOM</h3>
       </div>
