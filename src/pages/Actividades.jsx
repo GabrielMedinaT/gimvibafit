76b13039b193a6fcb160759e50cd69a5a62914c5
Daniel <danielojedamartel12@gmail.com>
2023-04-09 14:09:20 +0100

    JSX actividades preparado para movil

--- src/pages/Actividades.jsx
+++ src/pages/Actividades.jsx
@@ -85,84 +85,139 @@ const Actividades = () => {
           <h3>GAP</h3>
         </div>
       </div>
-      <div className="hm3" id="horas">
-        <h3>Pilates</h3>
+      <div className="hm3">
+        <div className="horasMov">
+          <h3>18:00</h3>
+        </div>
+        <div className="hm3" id="horas">
+          <h3>Pilates</h3>
+        </div>
       </div>
-      <div className="hm4" id="horas">
-        <h3>Trampolín</h3>
-        <h3>Dance Fit</h3>
-        <h3>Strong</h3>
-        <h3>Ritmos Mixtos</h3>
+      <div className="hm4">
+        <div>
+          <h3 className="horasMov">19:00</h3>
+        </div>
+        <div className="hm4" id="horas">
+          <h3>Trampolín</h3>
+          <h3>Dance Fit</h3>
+          <h3>Strong</h3>
+          <h3>Ritmos Mixtos</h3>
+        </div>
       </div>
       <div className="hm5"></div>
       {/* MIERCOLES */}
       <div className="dmiercoles" id="dia">
         <h3>Miercoles</h3>
       </div>
-      {/* <div> */}
-      {/* <div className="horasMov">
-          <h3>9:30</h3>
-        </div> */}
-      <div className="hx1" id="horas">
-        <h3>Cardio Box</h3>
-        <h3>Trampolín</h3>
-        <h3>Comba</h3>
-        <h3>Metcom</h3>
+      <div className="hx1">
+        <div>
+          <h3 className="horasMov">9:30</h3>
+        </div>
+        <div className="hx1" id="horas">
+          <h3>Cardio Box</h3>
+          <h3>Trampolín</h3>
+          <h3>Comba</h3>
+          <h3>Metcom</h3>
+        </div>
       </div>
-      {/* </div> */}
-      <div className="hx2" id="horas">
-        <h3>METCOM</h3>
+      <div className="hx2">
+        <div className="horasMov">
+          <h3>10:30</h3>
+        </div>
+        <div className="hx2" id="horas">
+          <h3>METCOM</h3>
+        </div>
       </div>
-      <div className="hx3" id="horas">
-        <h3>Traccion</h3>
-        <h3>Funcional</h3>
-        <h3>GAP</h3>
-        <h3>WOD</h3>
+      <div className="hx3">
+        <div className="horasMov">
+          <h3>18:00</h3>
+        </div>
+        <div className="hx3" id="horas">
+          <h3>Traccion</h3>
+          <h3>Funcional</h3>
+          <h3>GAP</h3>
+          <h3>WOD</h3>
+        </div>
       </div>
-      <div className="hx4" id="horas">
-        <h3>Merengefit</h3>
-        <h3>Bailame Fit</h3>
-        <h3>Circuito</h3>
-        <h3>Vive Bailando</h3>
+      <div className="hx4">
+        <div className="horasMov">
+          <h3>19:00</h3>
+        </div>
+        <div className="hx4" id="horas">
+          <h3>Merengefit</h3>
+          <h3>Bailame Fit</h3>
+          <h3>Circuito</h3>
+          <h3>Vive Bailando</h3>
+        </div>
       </div>
-      <div className="hx5" id="horas">
-        <h3>Zumba</h3>
-        <h3>Circuito</h3>
-        <h3>Vive Bailando</h3>
-        <h3>Funcional</h3>
+      <div className="hx5">
+        <div className="horasMov">
+          <h3>20:00</h3>
+        </div>
+        <div className="hx5" id="horas">
+          <h3>Zumba</h3>
+          <h3>Circuito</h3>
+          <h3>Vive Bailando</h3>
+          <h3>Funcional</h3>
+        </div>
       </div>
       {/* JUEVES */}
       <div className="djueves" id="dia">
         <h3>Jueves</h3>
       </div>
-      <div className="hj1" id="horas">
-        <h3>Metcom</h3>
-        <h3>4 Season</h3>
-        <h3>Hit</h3>
-        <h3>GAP</h3>
+      <div className="hj1">
+        <div className="horasMov">
+          <h3>09:30</h3>
+        </div>
+        <div className="hj1" id="horas">
+          <h3>Metcom</h3>
+          <h3>4 Season</h3>
+          <h3>Hit</h3>
+          <h3>GAP</h3>
+        </div>
       </div>
-      <div className="hj2" id="horas">
-        <h3>Body Up</h3>
+      <div>
+        <div className="horasMov">
+          <h3>10:30</h3>
+        </div>
+        <div className="hj2" id="horas">
+          <h3>Body Up</h3>
+        </div>
       </div>
-      <div className="hj3" id="horas">
-        <h3>Pilates</h3>
+      <div className="hj3">
+        <div className="horasMov">
+          <h3>18:00</h3>
+        </div>
+        <div className="hj3" id="horas">
+          <h3>Pilates</h3>
+        </div>
       </div>
-      <div className="hj4" id="horas">
-        <h3>Zumba</h3>
-        <h3>Puro Ritmo</h3>
-        <h3>Trampolín</h3>
-        <h3>Funcioal</h3>
+      <div className="hj4">
+        <div className="horasMov">
+          <h3>19:00</h3>
+        </div>
+        <div className="hj4" id="horas">
+          <h3>Zumba</h3>
+          <h3>Puro Ritmo</h3>
+          <h3>Trampolín</h3>
+          <h3>Funcioal</h3>
+        </div>
       </div>
 
       {/* Viernes */}
       <div className="dviernes" id="dia">
         <h3>Viernes</h3>
       </div>
-      <div className="hv1" id="horas">
-        <h3>Cada Viernes</h3>
-        <h3>una clase</h3>
-        <h3>diferente del</h3>
-        <h3>horario</h3>
+      <div className="hv1">
+        <div className="horasMov">
+          <h3>09:30</h3>
+        </div>
+        <div className="hv1" id="horas">
+          <h3>Cada Viernes</h3>
+          <h3>una clase</h3>
+          <h3>diferente del</h3>
+          <h3>horario</h3>
+        </div>
       </div>
       {/* Los siguientes div corresponden a las horas, seran tienen un className de la siguiente manera
       hora1, hora2 etc */}
